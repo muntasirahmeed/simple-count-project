69c94b1de62c74108d202ee64e60936312fe43ab
muntasir ahmeed <muntasirahmed5255@gmail.com>
2022-02-19 17:40:18 +0600

    counter done

--- tasbih.js
+++ tasbih.js
@@ -1,3 +1,20 @@
+function tasbih(getId, isAdding) {
+    let count = document.getElementById(getId);
+    let text = count.innerText;
+    let number = parseInt(text);
+    if (isAdding == true) {
+        count.innerText = number + 1;
+    }
+    else if(number>0) {
+        count.innerText = number - 1;
+    }
+}
+
+
 document.getElementById('+').addEventListener('click', () => {
-    console.log('plus click')
+   tasbih('count',true)
+})
+document.getElementById('-').addEventListener('click', () => {
+   
+    tasbih('count',false)
 })
